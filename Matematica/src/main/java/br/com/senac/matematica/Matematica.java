7d18e3b2e014168c58a09c060ed13f94a2794c61
PedroPauloSP <pedropsantospereira@gmail.com>
2025-08-04 21:04:37 -0300

    Remoção de comentários

---
+++ Matematica/src/main/java/br/com/senac/matematica/Matematica.java
@@ -0,0 +1,14 @@
+
+
+package br.com.senac.matematica;
+
+/**
+ *
+ * @author pedro
+ */
+public class Matematica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
